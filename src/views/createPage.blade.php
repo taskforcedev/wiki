c7af852ca3e1f51dab6bf35713aa127218662f27
PrivateSniper <privatesniper@gmail.com>
2015-11-08 16:33:37 +0000

    Add form groups

--- src/views/createPage.blade.php
+++ src/views/createPage.blade.php
@@ -4,14 +4,19 @@
     <h1>{{ $sitename }} - Create Wiki Page - {{ $page }}</h1>
 
     <form action="{{ route('wiki.page.create') }}" method="post">
-        <label for="title">Title</label>
-        <input class="form-control" type="text" name="title" value="{{ $page }}" />
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input class="form-control" type="text" name="title" value="{{ $page }}" />
+        </div>
 
-        <label for="url">URL</label>
-        <input class="form-control" type="text" name="url" value="{{ $page }}" />
+        <div class="form-group">
+            <label for="url">URL</label>
+            <input class="form-control" type="text" name="url" value="{{ $page }}" />
+        </div>
 
-        <label for="content">Content</label>
-        <textarea class="form-control" name="content" rows="20">
-        </textarea>
+        <div class="form-group">
+            <label for="content">Content</label>
+            <textarea class="form-control" name="content" rows="20"></textarea>
+        </div>
     </form>
 @endsection
