408b005aee77dd43e8172f0357a650643360391f
PrivateSniper <privatesniper@gmail.com>
2015-11-08 16:20:07 +0000

    Add route aliases, update create page.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,6 +4,7 @@ Route::group(['namespace' => 'Taskforcedev\Wiki\Http\Controllers'], function() {
 
     Route::get('wiki/install', ['as' => 'wiki.install', 'uses' => 'AdminController@install']);
 
-    Route::get('wiki/{page}', ['as' => 'page.view', 'uses' => 'WikiController@view']);
+    Route::get('wiki/{page}', ['as' => 'wiki.page.view', 'uses' => 'WikiController@view']);
+    Route::post('wiki/{page}', ['as' => 'wiki.page.create', 'uses' => 'WikiController@create']);
 
 });
--- src/views/createPage.blade.php
+++ src/views/createPage.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
     <h1>{{ $sitename }} - Create Wiki Page - {{ $page }}</h1>
+
+    <form action="{{ route('wiki.page.create') }}" method="post">
+        <label>Title</label>
+        <input type="text" name="title" />
+
+        <label>URL</label>
+        <input type="text" name="url" value="{{ $page }}" />
+
+        <label>Content</label>
+        <textarea name="content">
+        </textarea>
+    </form>
 @endsection
